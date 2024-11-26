e91e275c5a704cb607da348017b6b65655b2224f
shane-reaume <shanereaume@gmail.com>
2024-11-26 14:31:34 -0800

    add foundation of tests 3

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/integration/**/*.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
